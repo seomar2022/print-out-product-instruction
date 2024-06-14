b4b9b40817cf38607e2e34742521370b0d359a6e
SeoMar <118463057+seomar2022@users.noreply.github.com>
2024-06-14 18:37:11 +0900

    feat: Alert not founded file

--- print_out_product_sheet.py
+++ print_out_product_sheet.py
@@ -12,6 +12,7 @@
 from pypdf import PdfWriter #pip install pypdf #pdf병합기능 쓰기 위해.
 from datetime import datetime #병합된 pdf이름에 오늘 날짜 쓰기 위해.
 import pandas as pd #pip install pandas openpyxl #엑셀의 데이터를 읽어오기 위해.
+import pyautogui #pip install pyautogui
 
 # 엑셀 파일을 읽어오기
 df = pd.read_csv('data.csv', usecols=['상품코드'])
@@ -20,7 +21,7 @@ df = pd.read_csv('data.csv', usecols=['상품코드'])
 codes = df['상품코드'].tolist()
 
 #카페24의 상품코드만 가져오기위해서 P000로 시작하는 문자열 코드만 남겨둔다.
-#걸러지는 데이터) 스마트스토어 상품 코드(9로 시작하는 숫자), 톡스토어 상품코드(3으로 시작하는 숫자), 비어있는 셀(nan)
+#걸러지는 데이터) 스마트스토어 상품 코드(9, 10으로 시작하는 숫자), 톡스토어 상품코드(3으로 시작하는 숫자), 비어있는 셀(nan)
 #isinstance(item, str) ->item이 str이면 true를 반환
 cafe24_codes = [item for item in codes if isinstance(item, str) and item.startswith("P000")]
 
@@ -35,12 +36,13 @@ for code in cafe24_codes:
         file_not_found.append(code)
 
 
-print(file_not_found)
 # 현재 날짜 가져오기
 now = datetime.now().strftime("%m.%d.%a") #월.일.요일
 
 merge_pdf.write(f"{now}_product_sheet.pdf")
 merge_pdf.close()
+
+pyautogui.alert(file_not_found)
 
 #pypdf가 나을까 pywin32가 나을까?
 # 사용 예시
