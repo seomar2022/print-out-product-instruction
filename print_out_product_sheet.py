7fb852a464194db38d3416994bd0372750f3d525
SeoMar <118463057+seomar2022@users.noreply.github.com>
2024-07-08 17:38:44 +0900

    feat: Tell not found files

--- print_out_product_sheet.py
+++ print_out_product_sheet.py
@@ -1,14 +1,3 @@
-#각 상품 코드와 관리용 상품명 연결
-#설명지 한장씩 저장. 이름을 상품 코드로 지정
-#전채널 주문리스트를 입력. 그 중 상품코드 열만 가지고 오기.
-#필요한 설명지를 순서대로 pdf하나로 병합
-#인쇄 기능 넣어야하나?
-
-#https://chuun92.tistory.com/10
-#https://pypdf.readthedocs.io/en/stable/user/merging-pdfs.html
-#https://wikidocs.net/226862
-#https://wikidocs.net/153818
-
 from pypdf import PdfWriter #pip install pypdf #pdf병합기능 쓰기 위해.
 from datetime import datetime #병합된 pdf이름에 오늘 날짜 쓰기 위해.
 import pandas as pd #pip install pandas openpyxl #엑셀의 데이터를 읽어오기 위해.
@@ -48,17 +37,16 @@ for code in codes:
     elif code.startswith("3") : #카카오
         result = convert_to_cafe24(code, "kakao_code")
         converted_codes.append(result)
-        print("kakao")
 
 #### PDF 파일 병합
 merge_pdf = PdfWriter()
-file_not_found = []
+not_found_files = {}
 
 for converted_code in converted_codes:
     try:
         merge_pdf.append(f"sheets\\{converted_code}.pdf")
     except FileNotFoundError:
-        file_not_found.append(converted_code)
+        not_found_files[converted_code] = ''
 
 ####설명지없이 출고되는 상품인지 확인
 #일단 보류
@@ -69,7 +57,22 @@ now = datetime.now().strftime("%m.%d.%a") #월.일.요일
 merge_pdf.write(f"{now}_product_sheet.pdf")
 merge_pdf.close()
 
-####설명지 없는 파일 알려주기
-pyautogui.alert(file_not_found)
+####설명지 없는 상품코드와 상품명 알려주기
+#설명지 없는 상품의 코드를 전채널주문리스트에서 찾고, 해당 상품의 이름을 가져와서 딕셔너리의 값으로 넣기
+#매크로 돌리기 전의 열이름은 '상품명(한국어 쇼핑몰)' 돌린 후는 '상품명'이라서 상품명이 포함된 열을 지정
+product_name_col = [col for col in order_list.columns if "상품명" in col]
+for key in not_found_files:
+    key_in_order_list = order_list.query(f"상품코드 == @key")
+    not_found_files[key] = key_in_order_list[product_name_col[0]].iat[0]
+    
 
-####pywin32로 프린트
+#csv파일로 저장
+converted_codes_df = pd.DataFrame(list(not_found_files.items()), columns=['상품코드', '상품명'])
+converted_codes_df.to_csv(f"{now}_not_found_files.csv", index=False, encoding='utf-8-sig')
+
+####pyautogui로 프로그램 실행 결과 알려주기
+if len(not_found_files) == 0:
+    alert_msg = "모든 상품의 설명지를 찾았습니다!:)"
+else:
+    alert_msg=f"{len(not_found_files)}개의 설명지를 찾지 못했습니다:(\nnot_found_files을 확인해주세요"
+pyautogui.alert(text=alert_msg, title='실행 결과!', button='OK')
