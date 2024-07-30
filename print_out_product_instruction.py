8f7dcd95dc95ccaf3f7182311bdda665801278fa
SeoMar <118463057+seomar2022@users.noreply.github.com>
2024-07-30 13:35:35 +0900

    feat: Add GUI

--- print_out_product_instruction.py
+++ print_out_product_instruction.py
@@ -1,11 +1,28 @@
+import os
+import sys
 from pypdf import PdfWriter #pip install pypdf #pdf병합기능 쓰기 위해.
 from datetime import datetime #병합된 pdf이름에 오늘 날짜 쓰기 위해.
 import pandas as pd #pip install pandas openpyxl #엑셀의 데이터를 읽어오기 위해.
 import pyautogui #pip install pyautogui
+import xlwings as xw #매크로실행위해
+
+pyautogui.alert(text="설명지 병합 프로그램 v2.0.0입니다! ok버튼을 눌러 실행해주세요\n문의:seomar2022@gmail.com", title='시작!', button="ok")
 
 ####엑셀 파일 읽어오기
-# 전채널 주문리스트 파일을 읽어오기
-order_list = pd.read_csv('data.csv')
+# 전채널주문리스트가 담긴 폴더 읽어오기
+order_list_folder_name = "order_list"
+if os.path.isdir(order_list_folder_name):#폴더 있는지 확인
+    order_list_folder = os.listdir(order_list_folder_name)
+else:
+    os.makedirs(order_list_folder_name)
+
+# 전채널주문리스트 파일을 읽어오기
+if len(order_list_folder) != 0:
+    order_list_path = f"{order_list_folder_name}\\{order_list_folder[0]}"
+    order_list = pd.read_csv(order_list_path)
+else:
+    pyautogui.alert(f"{order_list_folder_name} 폴더에 파일이 없습니다!", button="프로그램 종료")
+    sys.exit() #프로그램 종료
 
 # 상품코드 열의 데이터를 문자열로 변환하고 NaN 값을 빈 문자열로 대체
 codes = order_list['상품코드'].astype(str).fillna("").tolist()
@@ -44,17 +61,18 @@ not_found_files = {}
 
 for converted_code in converted_codes:
     try:
-        merge_pdf.append(f"sheets\\{converted_code}.pdf")
+        merge_pdf.append(f"product_instruction\\{converted_code}.pdf")
     except FileNotFoundError:
         not_found_files[converted_code] = ''
 
 ####설명지없이 출고되는 상품인지 확인
 #일단 보류
 
+result_folder = "result"
 # 현재 날짜 가져오기
 now = datetime.now().strftime("%m.%d.%a") #월.일.요일
 
-merge_pdf.write(f"{now}_product_sheet.pdf")
+merge_pdf.write(f"{result_folder}\\{now}_product_instruction.pdf")
 merge_pdf.close()
 
 ####설명지 없는 상품코드와 상품명 알려주기
@@ -66,13 +84,45 @@ for key in not_found_files:
     not_found_files[key] = key_in_order_list[product_name_col[0]].iat[0]
     
 
-#csv파일로 저장
-converted_codes_df = pd.DataFrame(list(not_found_files.items()), columns=['상품코드', '상품명'])
-converted_codes_df.to_csv(f"{now}_not_found_files.csv", index=False, encoding='utf-8-sig')
+####전채널 주문리스트 매크로 실행
+#엑셀 모두 닫은 상태에서 시작해야할 듯.
+
+#매크로 실행
+try:
+    # 엑셀 애플리케이션 시작 및 파일 열기 (빈 통합 문서 생성을 방지)
+    app = xw.App(visible=True, add_book=False)
+    workbook = app.books.open(order_list_path)
+    
+    # personal.xlsb 파일 열기(매크로가 저장된 파일)
+    # 아마 여기는 각 컴퓨터에 맞춰서 별도로 지정해야할듯? ->초기설정으로 넣기
+    personal_wb = app.books.open(r'C:\Users\User\AppData\Roaming\Microsoft\Excel\XLSTART\PERSONAL.XLSB')
+    
+    # 주문리스트 파일을 활성화(매크로가 적용될 파일이므로)
+    workbook.activate()
+    
+    # 매크로 실행 (personal_wb에서 호출)
+    macro = personal_wb.macro('전채널주문리스트') 
+    macro()
+    
+    # 어차피 프린트만 하고 지우니까 저장안함. csv는 표시형식같은건 저장안되니까 닫으면 안됨..
+    # personal.xlsb 파일 닫기
+    personal_wb.close()
+    
+    print(f"매크로가 성공적으로 실행되었습니다.")
+    
+except Exception as e:
+    print(f"매크로 실행 중 오류가 발생했습니다: {e}")
 
 ####pyautogui로 프로그램 실행 결과 알려주기
 if len(not_found_files) == 0:
-    alert_msg = "모든 상품의 설명지를 찾았습니다!:)"
+    alert_msg = "모든 상품의 설명지를 찾았습니다!"
 else:
-    alert_msg=f"{len(not_found_files)}개의 설명지를 찾지 못했습니다:(\nnot_found_files을 확인해주세요"
-pyautogui.alert(text=alert_msg, title='실행 결과!', button='OK')
+    #csv파일로 저장
+    converted_codes_df = pd.DataFrame(list(not_found_files.items()), columns=['상품코드', '상품명'])
+    converted_codes_df.to_csv(f"{result_folder}\\{now}_not_found_files.csv", index=False, encoding='utf-8-sig')
+    alert_msg=f"{len(not_found_files)}개의 설명지를 찾지 못했습니다"
+    
+pyautogui.alert(text=alert_msg+" result폴더를 확인해주세요", title='실행 결과!', button='네!')
+os.startfile(result_folder) #폴더 열기
+
+#pyinstaller --onefile print_out_product_instruction.py
